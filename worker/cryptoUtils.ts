584d33f6165fdff1a6a13aa4f1539412c862a411
Dimitri <accept@protonmail.ch>
2024-07-29 20:29:48 +0200

    fix crypto

--- worker/cryptoUtils.ts
+++ worker/cryptoUtils.ts
@@ -4,16 +4,11 @@ export const sha256 = async (body: string): Promise<Uint8Array> => {
 	return new Uint8Array(hashBuffer);
 };
 
-export const hmacSha256 = async (
-	body: string,
-	secret: string | Uint8Array
-): Promise<Uint8Array> => {
+export const hmacSha256 = async (body: string, secret: string): Promise<Uint8Array> => {
 	const enc = new TextEncoder();
 	const algorithm = { name: 'HMAC', hash: 'SHA-256' };
-	if (!(secret instanceof Uint8Array)) {
-		secret = enc.encode(secret);
-	}
-	const key = await crypto.subtle.importKey('raw', secret, algorithm, false, ['sign', 'verify']);
+
+	const key = await crypto.subtle.importKey('raw', enc.encode(secret), algorithm, false, ['sign']);
 
 	const signature = await crypto.subtle.sign(algorithm.name, key, enc.encode(body));
 
@@ -21,10 +16,9 @@ export const hmacSha256 = async (
 };
 
 export const hex = (buffer: Uint8Array): string => {
-	const hashArray = Array.from(buffer);
-
-	// convert bytes to hex string
-	return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+	return Array.from(buffer)
+		.map(b => b.toString(16).padStart(2, '0'))
+		.join('');
 };
 
 export const generateSecret = (bytes: number): string => {
