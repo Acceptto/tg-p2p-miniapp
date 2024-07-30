0b5c71d5f4c8752c7a5734a00fd2d43f4e350f59
Dimitri <accept@protonmail.ch>
2024-07-30 15:00:24 +0200

    fix crypto

--- worker/index.ts
+++ worker/index.ts
@@ -160,7 +160,7 @@ router.get('/', (request: Request, app: App, env: Env) => {
 
 router.post('/', async (request: Request, app: App, env: Env) => {
 	const body = await request.text();
-	const xhub = new XHubSignature('sha256', env.INSTAGRAM_APP_SECRET);
+	const xhub = new XHubSignature('SHA-256', env.INSTAGRAM_APP_SECRET);
 	const signature = request.headers.get('X-Hub-Signature-256');
 
 	if (!signature) {
