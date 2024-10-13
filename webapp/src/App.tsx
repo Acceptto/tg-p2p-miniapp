7bdae2ddd012a5f7a2563934a32861a92e06350c
Dimitri <accept@protonmail.ch>
2024-10-13 13:15:50 +0200

    confetti

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,21 +1,23 @@
-import React from "react";
-import { Header } from "@/components/Header";
-import { Clock } from "@/components/Clock";
-import { Footer } from "@/components/Footer";
-import styled from "styled-components";
+import React from 'react';
+import { Header } from '@/components/Header';
+import { Clock } from '@/components/Clock';
+import { Footer } from '@/components/Footer';
+import { ConfettiEffect } from '@/components/ConfettiEffect';
+import styled from 'styled-components';
 
 const AppContainer = styled.div`
-  margin: 40vh 30vw;
+	margin: 40vh 30vw;
 `;
 
 const App: React.FC = () => {
-  return (
-    <AppContainer>
-      <Header />
-      <Clock />
-      <Footer />
-    </AppContainer>
-  );
+	return (
+		<AppContainer>
+			<ConfettiEffect />
+			<Header />
+			<Clock />
+			<Footer />
+		</AppContainer>
+	);
 };
 
 export default App;
---
+++ webapp/src/components/ConfettiEffect.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import confetti from 'canvas-confetti';
+
+export const ConfettiEffect: React.FC = () => {
+	useEffect(() => {
+		const scalar = 2;
+		const unicorn = confetti.shapeFromText({ text: '🦄', scalar });
+
+		const defaults = {
+			spread: 360,
+			ticks: 60,
+			gravity: 0,
+			decay: 0.96,
+			startVelocity: 20,
+			shapes: [unicorn],
+			scalar,
+		};
+
+		function shoot() {
+			confetti({
+				...defaults,
+				particleCount: 30,
+			});
+
+			confetti({
+				...defaults,
+				particleCount: 5,
+				flat: true,
+			});
+
+			confetti({
+				...defaults,
+				particleCount: 15,
+				scalar: scalar / 2,
+				shapes: ['circle'],
+			});
+		}
+
+		// Launch three bursts of confetti
+		setTimeout(shoot, 0);
+		setTimeout(shoot, 100);
+		setTimeout(shoot, 200);
+
+		// Clean up function (optional, as the confetti will finish on its own)
+		return () => {
+			confetti.reset();
+		};
+	}, []);
+
+	return null;
+};
