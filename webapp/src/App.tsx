cf7192326905682940889afeaafbd71e96b43100
Dimitri <accept@protonmail.ch>
2024-10-16 00:04:51 +0200

    fix

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -4,10 +4,13 @@ import Giveaway from '@/components/Giveaway';
 
 const App: React.FC = () => {
 	return (
-		<div className="container mx-auto p-4">
-			<ConfettiEffect />
-			<Giveaway />
-		</div>
+		<body className="m-0 font-sans antialiased">
+			<div className="container mx-auto p-4">
+				<ConfettiEffect />
+				<h1 className="text-3xl font-bold text-center mb-6">You are selected!</h1>
+				<Giveaway />
+			</div>
+		</body>
 	);
 };
 
